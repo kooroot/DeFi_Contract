0c0825d111be0868d108b8999b157fa4d42fe301
kooroot <koo.minjae@jinnhyun.com>
2022-10-05 18:12:45 +0900

    change os

---
+++ contracts/WalkerHill.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract WalkerHill is ERC20, ERC20Burnable, ERC20Snapshot, Ownable {
+    
+    uint constant _initial_supply = 100 * (10**18);
+
+    constructor() ERC20("WalkerHill", "WH") {
+        _mint(msg.sender, _initial_supply);
+    }
+
+    function snapshot() external {
+        _snapshot();
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Snapshot)
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+
+}
